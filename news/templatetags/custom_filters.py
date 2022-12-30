65cb6d46eb4c47a4c9ac721c59bfd1100898f564
ReutAS <reutas39@gmail.com>
2022-12-30 14:25:38 +0200

    filtr mod

--- news/templatetags/custom_filters.py
+++ news/templatetags/custom_filters.py
@@ -6,10 +6,31 @@ register = template.Library()
 
 # Регистрируем наш фильтр под именем currency, чтоб Django понимал,
 # что это именно фильтр для шаблонов, а не простая функция.
+
+
+WORDS = {
+    'редиска': 'редиска',
+    'директор': 'директор',
+    'глубине': 'глубине',
+    'поразили': 'поразили',
+    'убитое': 'убитое',
+    'напомним': 'напомним',
+    'чепухи': 'чепухи',
+    'простуды': 'простуды'
+}
+
+WOR = ['редиска', 'чепухи']
+
+
 @register.filter()
-def censored_func(value):
-   """
-   value: значение, к которому нужно применить фильтр
-   """
-   # Возвращаемое функцией значение подставится в шаблон.
-   return f'{value} Р'
+def censor(value):
+   a = value
+ #  for word in list(WORDS.keys()):
+   for word in WOR:
+      if word in a:
+         a = a.replace(word, f'{word[:1]}{len(word[1:])*"*"}')
+         #a = a.replace(word, f'{word[:1]}*****{word[-1:-2:-1]}')
+      if word.capitalize() in a:
+         a = a.replace(word.capitalize(), f'{word[:1].capitalize()}*****{word[-1:-2:-1].capitalize()}')
+
+   return a
