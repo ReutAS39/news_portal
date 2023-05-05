75868eee624de61aff3fe82d00e0a2428f9c8433
ReutAS <reutas39@gmail.com>
2023-05-05 15:16:39 +0200

    refactoring

--- news/views.py
+++ news/views.py
@@ -1,6 +1,5 @@
 from datetime import datetime
-# Импортируем класс, который говорит нам о том,
-# что в этом представлении мы будем выводить список объектов из БД
+
 from django.contrib import messages
 from django.shortcuts import redirect, render, reverse, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
@@ -45,7 +44,6 @@ def subscribe_me(request, pk):
 
 
 class PostsList(ListView):
-    # Указываем модель, объекты которой мы будем выводить
     model = Post
     # Поле, которое будет использоваться для сортировки объектов
     ordering = '-time_in'
@@ -54,7 +52,6 @@ class PostsList(ListView):
     template_name = 'news_list.html'
     # Это имя списка, в котором будут лежать все объекты.
     # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
-    # context_object_name = 'posts'
     context_object_name = 'news_list'
     paginate_by = 10  # вот так мы можем указать количество записей на странице
 
@@ -80,8 +77,6 @@ class PostsList(ListView):
         # Добавляем в контекст объект фильтрации.
         context['filterset'] = self.filterset
         # Добавим ещё одну пустую переменную,
-        # чтобы на её примере рассмотреть работу ещё одного фильтра.
-        context['next_sale'] = None
         context['title'] = 'Главная страница'
         context['cat_selected'] = 0
         context['menu'] = menu
@@ -99,9 +94,6 @@ class PostDetail(DetailView):
     context_object_name = 'news'
 
     def get_context_data(self, **kwargs):
-        # С помощью super() мы обращаемся к родительским классам
-        # и вызываем у них метод get_context_data с теми же аргументами, что и были переданы нам.
-        # В ответе мы должны получить словарь.
         context = super().get_context_data(**kwargs)
         context['menu'] = menu
         context['title'] = f"{context['news'].article}"
@@ -117,7 +109,6 @@ class PostCreate(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     # Указываем нашу разработанную форму
     form_class = PostForm
     model = Post
-    # и новый шаблон, в котором используется форма.
     template_name = 'news_edit.html'
 
     def get_context_data(self, **kwargs):
