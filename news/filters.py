d426dfd1caeb867a6fccca4f1b324a2cef3d5dae
ReutAS <reutas39@gmail.com>
2024-09-11 16:04:35 +0200

    filter modif

--- news/filters.py
+++ news/filters.py
@@ -1,15 +1,21 @@
-from django.forms import DateInput
-from django_filters import FilterSet, DateFilter
-from .models import Post
+from random import choices
 
-# Создаем свой набор фильтров для модели Product.
+from django.forms import DateInput
+from django_filters import FilterSet, DateFilter, ChoiceFilter
+
+from .models import Post, POSITION
+
+
+# Создаем свой набор фильтров для модели Post.
 # FilterSet, который мы наследуем,
 # должен чем-то напомнить знакомые вам Django дженерики.
 
 
 class PostFilter(FilterSet):
+    position = ChoiceFilter(choices=POSITION)
     # выводит виджет календаря при выборе даты за счёт атрибуты widget
-    time_in = DateFilter(lookup_expr='gt', widget=DateInput(format='%d/%m/%Y', attrs={'type': 'date'}))
+    time_in = DateFilter(lookup_expr='gt', widget=DateInput(attrs={'type': 'date'}))
+
 
     class Meta:
         model = Post
@@ -17,11 +23,13 @@ class PostFilter(FilterSet):
            # поиск по названию
            'article': ['icontains'],
            # exact выбирает все объекты моделей, в которых свойство равно определенному значению.
-           'author': ['exact'],
-                  }
+           'author__user__username': ['icontains'],
+        }
 
     def __init__(self, *args, **kwargs):
         super(PostFilter, self).__init__(*args, **kwargs)
-        self.filters['article__icontains'].label = "Заголовок"  # не меняется
-        self.filters['author'].label = "Автор"
-        self.filters['time_in'].label = "Дата"
+        self.filters['position'].label = "Статья/новость"
+        self.filters['article__icontains'].label = "Заголовок"
+        self.filters['author__user__username__icontains'].label = "Автор"
+        self.filters['time_in'].label = "За период с"
+
